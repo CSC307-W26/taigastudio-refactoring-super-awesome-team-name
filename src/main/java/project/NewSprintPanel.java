d6e4ccea3d555cbbbf6c72d6d2f6239c41a5f7ae
RoomDoom01 <85605670+RoomDoom01@users.noreply.github.com>
2026-02-03 14:44:32 -0800

    Create NewSprintPanel.java

---
+++ src/main/java/project/NewSprintPanel.java
@@ -0,0 +1,55 @@
+import javax.swing.*;
+import java.awt.*;
+import java.util.Date;
+
+/**
+ * Screen to enter information about a new Sprint being added to the project
+ *
+ * Finley Room
+ * version 1.0
+ */
+public class NewSprintPanel extends JFrame {
+    private JTextField name;
+    private JTextField startDate;
+    private JTextField endDate;
+    private JButton makeSprint;
+
+    public NewSprintPanel(){
+        super("Create New Sprint");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new FlowLayout());
+
+        JPanel row1=new JPanel(new FlowLayout());
+        name=new JTextField(30);
+        row1.add(new JLabel("Name:"));
+        row1.add(name);
+
+        JPanel row2=new JPanel(new FlowLayout());
+        startDate=new JTextField(10);
+        endDate=new JTextField(10);
+        row2.add(new JLabel("Start Date: "));
+        row2.add(startDate);
+        row2.add(new JLabel("End Date: "));
+        row2.add(endDate);
+
+        JPanel row3=new JPanel(new FlowLayout());
+        makeSprint=new JButton("Create Sprint");
+        makeSprint.addActionListener(e->submit());
+
+        add(row1);
+        add(row2);
+        add(row3);
+
+        setVisible(true);
+    }
+
+
+    public void submit(){
+        String sprintName=name.getText();
+        String start=startDate.getText();
+        String end=endDate.getText();
+        Date startDate=new Date(start);
+        Date endDate=new Date(end);
+        BlackBoard.blackboard.addSprint(new dao.Sprint(startDate, endDate));
+    }
+}
