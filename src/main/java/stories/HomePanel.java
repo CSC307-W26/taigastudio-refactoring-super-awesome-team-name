5708980e54700ce30751e073003f5f105297801a
Jonathan A Garcia <jonathanagarcia0821@gmail.com>
2026-02-05 11:47:27 -0800

    Add files via upload
    This class shows the home screen with the backlog list and the button to open the new story screen

---
+++ src/main/java/stories/HomePanel.java
@@ -0,0 +1,29 @@
+import stories.Backlog;
+import stories.NewStoryListener;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * This class shows the home screen with the backlog list and the button to open the new story screen
+ *
+ * @author Jonathan Garcia
+ * @version 1.0
+ */
+
+public class HomePanel extends JPanel {
+
+    public HomePanel(CardLayout cards, JPanel root, Backlog backlog) {
+        setLayout(new BorderLayout());
+
+        JButton newStoryBtn = new JButton("+ USER STORY");
+        newStoryBtn.setActionCommand("+ USER STORY");
+
+        newStoryBtn.addActionListener(new NewStoryListener(backlog));
+
+        newStoryBtn.addActionListener(e -> cards.show(root, "NEW"));
+
+        add(newStoryBtn, BorderLayout.NORTH);
+        add(new JScrollPane(backlog.getStoryList()), BorderLayout.CENTER);
+    }
+}
