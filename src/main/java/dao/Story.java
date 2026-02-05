383aff5ff7d018b6f59ec0fb53c8af0c90c8ae8f
Jonathan A Garcia <jonathanagarcia0821@gmail.com>
2026-02-05 11:39:51 -0800

    Update Story.java

--- src/main/java/dao/Story.java
+++ src/main/java/dao/Story.java
@@ -4,34 +4,47 @@ package dao;
  * This class represents a user story
  *
  * @author Jonathan Garcia
- * @version 1.0
+ * @version 2.0
  */
-public class Story {
-	
-	private String subject;
-	private String description;
-	private int score;
-	
-	public Story(String subject, String description, int score) {
-		this.subject = subject;
-		this.description = description;
-		this.score = score;
-	}
-	
-	public String getSubject() {
-		return subject;
-	}
-	
-	public String getDescription() {
-		return description;
-	}
-	
-	public int getScore() {
-		return score;
-	}
-	
-	public boolean getStoryAttribute(String id) {
-		return false;
-	}
 
+public class Story {
+
+    private final String title;
+    private final String description;
+    private final int points;
+    private final String status;
+    private final int priority;
+
+    public Story(String title, String description, int points, String status, int priority) {
+        this.title = title;
+        this.description = description;
+        this.points = points;
+        this.status = status;
+        this.priority = priority;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    @Override
+    public String toString() {
+        return "P" + priority + " | " + title + " (" + points + ") [" + status + "]";
+    }
 }
