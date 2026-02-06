8bb0a7bc2ceebe59116f48537d378ab96868f0d3
JohnnyParini <johnnyparini@gmail.com>
2026-02-05 23:46:09 -0800

    Updated main var in MouseNanny to be local. Integrated ListProjectHomePanel with EditPanel and BacklogWindow. Edited Project to reference Backlog as parameter. Began making the final integrated Main class.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,28 +1,34 @@
 
 
+import project.EditProjectPanelNanny;
+import project.ListProjectHomePanel;
 import stories.NewStoryNanny;
 import stories.NewStoryPanel;
 
 import javax.swing.*;
+import java.awt.*;
 
 /**
  * This class creates app window and connects the nanny
  *
- * @author Jonathan Garcia
+ * @author Johnny Parini
  * @version 1.0
  */
 public class Main extends JFrame {
 	
 	public Main() {
-		NewStoryNanny newStoryNanny = new NewStoryNanny(this);
-		NewStoryPanel newStoryPanel = new NewStoryPanel(newStoryNanny);
-		add(newStoryPanel);
+		EditProjectPanelNanny editNanny = new EditProjectPanelNanny(this);
+		ListProjectHomePanel home = new ListProjectHomePanel(this);
+		add(home, BorderLayout.CENTER);
 	}
 	
 	public static void main(String[] args) {
+
+
 		Main main = new Main();
+		main.setSize(1470, 600);
+		main.setTitle("Projects");
 		main.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		main.setSize(800, 800);
 		main.setVisible(true);
 	}
 }
