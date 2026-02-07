a4749a6d78d0add153ce39907da8b39a58fd1eb7
Jonathan A Garcia <jonathanagarcia0821@gmail.com>
2026-02-06 16:02:13 -0800

    Story v.03

--- src/main/java/dao/Story.java
+++ src/main/java/dao/Story.java
@@ -4,7 +4,7 @@ package dao;
  * This class represents a user story
  *
  * @author Jonathan Garcia
- * @version 2.0
+ * @version 3.0
  */
 
 public class Story {
@@ -14,6 +14,7 @@ public class Story {
     private final int points;
     private final String status;
     private final int priority;
+
     private final int ID;
     private static int countStories = 0;
 
@@ -23,19 +24,23 @@ public class Story {
         this.points = points;
         this.status = status;
         this.priority = priority;
-        ID = ++countStories;
+        this.ID = ++countStories;
+    }
+
+    public int getID() {
+        return ID;
     }
 
     public String getTitle() {
         return title;
     }
 
-    public String getDescription() {
-        return description;
-    }
-
     public int getPoints() {
         return points;
+    }
+
+    public String getDescription() {
+        return description;
     }
 
     public String getStatus() {
@@ -45,8 +50,6 @@ public class Story {
     public int getPriority() {
         return priority;
     }
-
-    public int getID() { return ID; }
 
     @Override
     public String toString() {
